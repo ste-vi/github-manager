081a5d7555c0bc47a91c947c1986b16eab3f251d
ste-vi <trozzato@gmail.com>
2024-02-12 18:11:53 +0200

    [BE] project initial

---
+++ backend/src/main/java/com/stevi/githubmanager/GithubManagerApplication.java
@@ -0,0 +1,13 @@
+package com.stevi.githubmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubManagerApplication.class, args);
+	}
+
+}

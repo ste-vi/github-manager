94aab85665969c04e46a47fefc705eb236d57d79
ste-vi <trozzato@gmail.com>
2024-02-21 11:24:03 +0200

    [UI] created add repository modal component

---
+++ frontend/src/app/components/dashboard/modal/add-repository-modal/add-repository-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-repository-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-repository-modal.component.html',
+  styleUrl: './add-repository-modal.component.scss'
+})
+export class AddRepositoryModalComponent {
+
+}

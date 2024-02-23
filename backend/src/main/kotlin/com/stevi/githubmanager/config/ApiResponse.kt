9442bbb073b14e028a4b9875baa9112ff186f490
ste-vi <trozzato@gmail.com>
2024-02-23 09:28:55 +0200

    [BE] added exception handler

---
+++ backend/src/main/kotlin/com/stevi/githubmanager/config/ApiResponse.kt
@@ -0,0 +1,5 @@
+package com.stevi.githubmanager.config
+
+data class ApiResponse(val message: String) {
+
+}
---
+++ backend/src/main/kotlin/com/stevi/githubmanager/config/ExceptionHandler.kt
@@ -0,0 +1,26 @@
+package com.stevi.githubmanager.config
+
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.http.HttpStatus
+import com.stevi.githubmanager.exception.ResourceNotFoundException
+
+@ControllerAdvice
+class ExceptionHandler {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ResourceNotFoundException::class)
+    @ResponseBody
+    fun handleResourceNotFoundException(ex: ResourceNotFoundException): ApiResponse {
+        return ApiResponse(ex.message!!)
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception::class)
+    @ResponseBody
+    fun handleException(ex: Exception): ApiResponse {
+        return ApiResponse("Something went wrong")
+    }
+}
